78094a633c38c487dce08da1a2faf98e83c20767
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-16 22:30:56 +0530

    Add password checker program

--- Week-02/Week-02/Programs/password_checker.py
+++ Week-02/Week-02/Programs/password_checker.py
@@ -1,1 +1,11 @@
+# Password Checker Program
+# Day 9 - Python if-else
 
+correct_password = "python123"
+
+password = input("Enter your password: ")
+
+if password == correct_password:
+    print("Access Granted ✅")
+else:
+    print("Access Denied ❌")
