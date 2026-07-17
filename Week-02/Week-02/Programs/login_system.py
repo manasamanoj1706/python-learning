dc2c2bb5767142fc61386883f1c24b623d3c4dc2
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-17 22:36:40 +0530

    Implement basic login system with nested if statements

--- Week-02/Week-02/Programs/login_system.py
+++ Week-02/Week-02/Programs/login_system.py
@@ -1,1 +1,16 @@
+# Login System
+# Day 10 - Nested if Statements
 
+correct_username = "admin"
+correct_password = "python123"
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == correct_username:
+    if password == correct_password:
+        print("Login Successful ✅")
+    else:
+        print("Wrong Password ❌")
+else:
+    print("Username Not Found ❌")
