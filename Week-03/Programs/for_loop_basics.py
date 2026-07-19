92704e6ae7ba0252847baaa06e91668dbf60c357
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-19 12:12:12 +0530

    Add for loop basics example in Python
    This script demonstrates the basics of for loops in Python, including printing numbers, strings, and even/odd numbers.

---
+++ Week-03/Programs/for_loop_basics.py
@@ -0,0 +1,25 @@
+# =========================================
+# Python Learning - Day 12
+# Topic: For Loop Basics
+# Author: Manasa Manoj
+# =========================================
+
+print("=== FOR LOOP BASICS ===")
+
+print("\nNumbers from 1 to 5")
+for i in range(1, 6):
+    print(i)
+
+print("\nPrinting 'Python' 5 times")
+for i in range(5):
+    print("Python")
+
+print("\nEven numbers from 2 to 10")
+for i in range(2, 11, 2):
+    print(i)
+
+print("\nOdd numbers from 1 to 9")
+for i in range(1, 10, 2):
+    print(i)
+
+print("\nLoop Completed Successfully!")
