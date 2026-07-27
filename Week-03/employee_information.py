759104525da1bbcd5df8a1cd9576bd67f3a53ff7
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-27 23:00:33 +0530

    Add employee information data structure

---
+++ Week-03/employee_information.py
@@ -0,0 +1,7 @@
+employees = [
+    (1001, "Rahul", "Software Engineer", "IT", 50000),
+    (1002, "Anu", "Data Analyst", "AI", 48000),
+    (1003, "Manasa", "AI Engineer", "AI", 65000),
+    (1004, "Arun", "HR Manager", "HR", 42000),
+    (1005, "Sneha", "Developer", "IT", 55000)
+]
