b6f1b1b0fb623bfef094de303d91c58e4bf993f2
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-28 23:46:25 +0530

    Add student record management with sample data

---
+++ Week-03/student_record_management.py
@@ -0,0 +1,17 @@
+students = {
+    101: {
+        "name": "Manasa",
+        "department": "AI & DS",
+        "cgpa": 8.2
+    },
+    102: {
+        "name": "Rahul",
+        "department": "CSE",
+        "cgpa": 7.8
+    },
+    103: {
+        "name": "Anu",
+        "department": "ECE",
+        "cgpa": 8.6
+    }
+}
