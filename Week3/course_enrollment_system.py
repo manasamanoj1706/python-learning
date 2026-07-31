80c2b7f097ddaab5513978064ba49eef0a11aabe
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-31 21:18:45 +0530

    Add student enrollment data to course system

---
+++ Week3/course_enrollment_system.py
@@ -0,0 +1,12 @@
+students = {
+    101: {
+        "name": "Manasa",
+        "course": "Python",
+        "duration": "3 Months"
+    },
+    102: {
+        "name": "Rahul",
+        "course": "Machine Learning",
+        "duration": "6 Months"
+    }
+}
