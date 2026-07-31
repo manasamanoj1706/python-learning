5b31d13a448ed8483a64661818bfc8b1d734483b
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-07-31 21:17:59 +0530

    Add inventory management system with initial items

---
+++ Week3/inventory_management_system.py
@@ -0,0 +1,12 @@
+inventory = {
+    101: {
+        "name": "Keyboard",
+        "price": 1200,
+        "quantity": 15
+    },
+    102: {
+        "name": "Mouse",
+        "price": 700,
+        "quantity": 20
+    }
+}
