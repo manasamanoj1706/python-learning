5f5722d8146e631b683a30d22780d13665ac22a1
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-01 23:11:24 +0530

    Add initial online shopping system structure

---
+++ Week3/online_shopping_system.py
@@ -0,0 +1,16 @@
+users = {
+    "manasa": {
+        "password": "1234",
+        "cart": [],
+        "orders": []
+    }
+}
+
+products = {
+    101: {
+        "name": "Laptop",
+        "price": 65000,
+        "stock": 8,
+        "rating": 4.8
+    }
+}
