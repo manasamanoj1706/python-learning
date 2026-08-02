0b6bfa8904c21457f0fe83a36122bcb0ae61e169
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-02 21:35:38 +0530

    Create Employee Directory Management System.py

---
+++ Week3/Employee Directory Management System.py
@@ -0,0 +1,18 @@
+employees = {
+    1001: {
+        "name": "Rahul",
+        "department": "IT",
+        "designation": "Software Engineer",
+        "salary": 55000,
+        "email": "rahul@gmail.com",
+        "phone": "9876543210"
+    },
+    1002: {
+        "name": "Manasa",
+        "department": "AI & DS",
+        "designation": "AI Engineer",
+        "salary": 65000,
+        "email": "manasa@gmail.com",
+        "phone": "9123456780"
+    }
+}
