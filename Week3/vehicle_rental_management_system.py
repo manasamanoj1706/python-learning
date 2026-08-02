bf9d4781b01b00a12c2ee095828c9b1554d0e488
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-02 21:36:10 +0530

    Create vehicle_rental_management_system.py

---
+++ Week3/vehicle_rental_management_system.py
@@ -0,0 +1,24 @@
+vehicles = {
+    "V101": {
+        "brand": "Hyundai",
+        "model": "Creta",
+        "type": "SUV",
+        "price_per_day": 2500,
+        "available": True
+    },
+    "V102": {
+        "brand": "Honda",
+        "model": "City",
+        "type": "Sedan",
+        "price_per_day": 1800,
+        "available": False
+    }
+}
+
+customers = {
+    "C101": {
+        "name": "Manasa",
+        "phone": "9876543210",
+        "rented_vehicle": "V102"
+    }
+}
