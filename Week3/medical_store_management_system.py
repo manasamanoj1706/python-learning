9998d368d9e2716e6c41d2e198d6c8a064d29572
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-03 22:24:23 +0530

    Create medical_store_management_system.py

---
+++ Week3/medical_store_management_system.py
@@ -0,0 +1,16 @@
+medicines = {
+    "M101": {
+        "name": "Paracetamol",
+        "price": 25,
+        "stock": 120,
+        "expiry": "2027-08-15"
+    }
+}
+
+customers = {
+    "C101": {
+        "name": "Manasa",
+        "phone": "9876543210",
+        "purchase_history": []
+    }
+}
