3aa68b7e0f207e140066a1c403634d3459cb5cda
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-04 23:11:12 +0530

    Create railway_reservation_system.py

---
+++ Week3/railway_reservation_system.py
@@ -0,0 +1,18 @@
+trains = {
+    "12637": {
+        "name": "Pandian Express",
+        "source": "Chennai",
+        "destination": "Madurai",
+        "available_seats": 120,
+        "fare": 650
+    }
+}
+
+passengers = {
+    "P101": {
+        "name": "Manasa",
+        "age": 21,
+        "gender": "Female",
+        "bookings": []
+    }
+}
