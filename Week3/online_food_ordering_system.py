8e55ed09d65bd83bf6e4654713e5a2bc545ac4bd
manasamanoj1706 <manasamanoj1706@gmail.com>
2026-08-04 23:11:45 +0530

    Create online_food_ordering_system.py

---
+++ Week3/online_food_ordering_system.py
@@ -0,0 +1,28 @@
+restaurants = {
+    "R101": {
+        "name": "Spice Garden",
+        "location": "Kochi",
+        "rating": 4.7,
+        "menu": {
+            "F101": {
+                "name": "Chicken Biryani",
+                "price": 180,
+                "stock": 50
+            },
+            "F102": {
+                "name": "Paneer Butter Masala",
+                "price": 160,
+                "stock": 30
+            }
+        }
+    }
+}
+
+customers = {
+    "C101": {
+        "name": "Manasa",
+        "phone": "9876543210",
+        "cart": [],
+        "orders": []
+    }
+}
